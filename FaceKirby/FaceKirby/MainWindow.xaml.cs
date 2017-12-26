e5def9853458aa0d920ebf10bd3970e5ddbfcd7a
Keiho Sakapon <git@saka-pon.net>
2017-12-26 10:37:45 +0900

    Set features to window

--- FaceKirby/FaceKirby/MainWindow.xaml.cs
+++ FaceKirby/FaceKirby/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using KLibrary.Labs.UI;
 
 namespace FaceKirby
 {
@@ -23,6 +24,10 @@ namespace FaceKirby
         public MainWindow()
         {
             InitializeComponent();
+
+            this.SetBorderless();
+            this.SetInactive();
+            MouseLeftButtonDown += (o, e) => DragMove();
         }
     }
 }

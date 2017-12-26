57feb11fe2072fbb7c097093798f0cb7408796fc
Keiho Sakapon <git@saka-pon.net>
2017-12-26 10:45:48 +0900

    Map buttons to keys

--- FaceKirby/FaceKirby/MainWindow.xaml.cs
+++ FaceKirby/FaceKirby/MainWindow.xaml.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using KLibrary.Labs.UI;
+using KLibrary.Labs.UI.Input;
+using Reactive.Bindings;
 
 namespace FaceKirby
 {
@@ -21,6 +25,23 @@ namespace FaceKirby
     /// </summary>
     public partial class MainWindow : Window
     {
+        static readonly SolidColorBrush NormalBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#99FF9933"));
+        static readonly SolidColorBrush PressedBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#99FF3300"));
+
+        static readonly Dictionary<string, Keys> KeyMap = new Dictionary<string, Keys>
+        {
+            { "⬆", Keys.I },
+            { "⬇", Keys.M },
+            { "⬅", Keys.J },
+            { "➡", Keys.K },
+            { "SELECT", Keys.A },
+            { "START", Keys.Return },
+            { "B", Keys.Z },
+            { "A", Keys.X },
+        };
+
+        List<ReactiveProperty<bool>> PressedList = new List<ReactiveProperty<bool>>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,6 +49,28 @@ namespace FaceKirby
             this.SetBorderless();
             this.SetInactive();
             MouseLeftButtonDown += (o, e) => DragMove();
+
+            var buttons = ButtonsPanel.Children.OfType<TextBlock>();
+            foreach (var button in buttons)
+            {
+                var key = KeyMap[button.Text];
+
+                var pressed = new ReactiveProperty<bool>();
+                PressedList.Add(pressed);
+
+                pressed
+                    .Do(b => button.Background = b ? PressedBrush : NormalBrush)
+                    .Subscribe(b =>
+                    {
+                        if (b)
+                            KeyboardInjection.KeyDown(key);
+                        else
+                            KeyboardInjection.KeyUp(key);
+                    });
+
+                button.TouchDown += (o, e) => pressed.Value = true;
+                button.TouchUp += (o, e) => pressed.Value = false;
+            }
         }
     }
 }
